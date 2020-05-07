75dca9e42f4d08899a458982c954f73037849236
wenju xing <linuxing3@qq.com>
2020-05-07 17:15:22 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,31 +1,26 @@
 #!/bin/bash
 
-DEST_DIR="${HOME}/tmp_repo"
+set -e
+
+DEST_DIR="${HOME}/dest_repo"
 DEST_URL="https://x-access-token:${INPUT_DESTINATION_TOKEN}@github.com/${INPUT_DESTINATION_REPO}.git"
 
-mkdir $DEST_DIR
+git clone --depth=1 --single-branch --branch master \
+  ${DEST_URL} \
+  ${DEST_DIR}
 
-# 将hugo源码拷贝到新位置
-cp -R \
-  ${GITHUB_WORKSPACE}/* \
-  ${DEST_DIR}
-cp -R \
-  ${GITHUB_WORKSPACE}/* \
-  ${DEST_DIR}
-  
-# 转到新目录
-cd ${DEST_DIR}
-# 编译生成静态网站文件
 hugo ${INPUT_HUGO_ARGS}
 
-# 进入发布目录
-cd ${INPUT_BUILD_DIR}/
-git init
+cp -R \
+  ${GITHUB_WORKSPACE}/${INPUT_BUILD_DIR}/* \
+  ${DEST_DIR}
+cp -R \
+  ${GITHUB_WORKSPACE}/${INPUT_BUILD_DIR}/.* \
+  ${DEST_DIR}
+
+cd ${DEST_DIR}
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git add --all
-git commit -m "Automated deployment by ${GITHUB_ACTOR} at ${TS}"
-git push -u origin master
-cd
-echo "Done deploy!"
-rm -rf $DEST_DIR
+git commit -m "Automated deployment"
+git push
