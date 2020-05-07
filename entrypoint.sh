3b32f5cd72579a715390df65767181b04af1454f
wenju xing <linuxing3@qq.com>
2020-05-07 15:54:07 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,7 +16,7 @@ git remote add origin "https://github.com/${INPUT_DESTINATION_REPO}.git"
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git add --all
-git push origin --delete master
+git checkout -b master
 git commit -m "Automated deployment by ${GITHUB_ACTOR} at ${TS}"
 git push -u origin master
 cd
