2c3a37298ec6d93bbe472fefab065357ccf1f511
wenju xing <linuxing3@qq.com>
2020-05-07 16:05:27 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-DEST_DIR="${HOME}/dest_repo"
-DEST_URL="https://x-access-token:${INPUT_DESTINATION_TOKEN}@github.com/${INPUT_DESTINATION_REPO}.git"
-TS=$(date +"%Y%m%d %H:%M")
-git clone --depth=1 --single-branch --branch source \
-  ${DEST_URL} \
-  ${DEST_DIR}
-
-cd ${DEST_DIR}/
-hugo ${INPUT_HUGO_ARGS}
-
 cd ${INPUT_BUILD_DIR}/
 git init
 git remote add origin "https://github.com/${INPUT_DESTINATION_REPO}.git"
