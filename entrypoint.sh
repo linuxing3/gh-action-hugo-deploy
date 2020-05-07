85cfa1f6e0c5ad7bb49f342f9143da9077d021e9
wenju xing <linuxing3@qq.com>
2020-05-07 14:43:05 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,22 +5,16 @@ set -e
 DEST_DIR="${HOME}/dest_repo"
 DEST_URL="https://x-access-token:${INPUT_DESTINATION_TOKEN}@github.com/${INPUT_DESTINATION_REPO}.git"
 
-git clone --depth=1 --single-branch --branch master \
+git clone --depth=1 --single-branch --branch source \
   ${DEST_URL} \
   ${DEST_DIR}
 
+cd ${DEST_DIR}/
 hugo ${INPUT_HUGO_ARGS}
 
-cp -R \
-  ${GITHUB_WORKSPACE}/${INPUT_BUILD_DIR}/* \
-  ${DEST_DIR}
-cp -R \
-  ${GITHUB_WORKSPACE}/${INPUT_BUILD_DIR}/.* \
-  ${DEST_DIR}
-
-cd ${DEST_DIR}
+cd ${INPUT_BUILD_DIR}/
 git init
-git remote add origin "https://github.com/${INPUT_DESTINATION_REPO}"
+git remote add origin "https://github.com/${INPUT_DESTINATION_REPO}.git"
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git add --all
