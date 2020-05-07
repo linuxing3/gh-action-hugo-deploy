ee3a5e38df9805d1c6efd818ab930619780bd3bc
wenju xing <linuxing3@qq.com>
2020-05-07 16:39:15 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-DEST_DIR="${HOME}/dest_repo"
+DEST_DIR="${HOME}/tmp_repo"
 DEST_URL="https://x-access-token:${INPUT_DESTINATION_TOKEN}@github.com/${INPUT_DESTINATION_REPO}.git"
+
+mkdir $DEST_DIR
 
 # 将hugo源码拷贝到新位置
 cp -R \
@@ -15,10 +17,10 @@ cp -R \
 cd ${DEST_DIR}
 # 编译生成静态网站文件
 hugo ${INPUT_HUGO_ARGS}
+
 # 进入发布目录
 cd ${INPUT_BUILD_DIR}/
 git init
-git remote add origin "https://github.com/${INPUT_DESTINATION_REPO}.git"
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git add --all
@@ -26,3 +28,4 @@ git commit -m "Automated deployment by ${GITHUB_ACTOR} at ${TS}"
 git push -u origin master
 cd
 echo "Done deploy!"
+rm -rf $DEST_DIR
